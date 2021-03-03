b2ec3eef221b8e34f0ac6a74474c50f788e00530
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-03-03 07:55:36 -0500

    fixed the table error

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,18 @@
-DROP DATABASE IF EXISTS companyEmployees;
-CREATE DATABASE companyEmployees;
-USE companyEmployees;
+DROP DATABASE IF EXISTS company_employees;
+CREATE DATABASE company_employees;
+USE company_employees;
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT,
     names VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT,
     title VARCHAR(50) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
-    department_id INTEGER UNSIGNED ,
+    department_id INTEGER,
     PRIMARY KEY (id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
 );
@@ -21,8 +21,8 @@ CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    roles_id INTEGER UNSIGNED ,
-    manager_id INTEGER UNSIGNED ,
+    roles_id INTEGER,
+    manager_id INTEGER,
     PRIMARY KEY (id),
     CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id),
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) 
