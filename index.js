533a38963c456f7acd67746b5e1a6f8f55c33c33
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-03-04 21:08:36 -0500

    all main questions added

--- index.js
+++ index.js
@@ -13,27 +13,65 @@ const connection = mysql.createConnection({
 connection.connect(err => {
     if(err) throw err;
     console.log('connected as id ' + connection.threadId);
-    output();
 });
 
-/////////////////////////
-inquirer
-.prompt([
-    /* Pass your questions in here */
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
+const inqStart = function(){
+  inquirer
+      .prompt([
+          {
+              type: 'list',
+              name: 'allOptions',
+              message: 'What please choose which action you would like to perform.',
+              choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 
+              'add a role', 'add an employee', 'update an employee role', 'exit' /*EXTRA*/]
+          }
+      ])
+      .then(ans => {
+          console.log(ans.allOptions);
+          switch(ans.allOptions){
+            case 'view all departments': 
+              console.log('I made the connection!!!departments');
+              dbAllDept();
+              break;
+            case 'view all roles': 
+              console.log('I made the connection!!!roles');
+              break;
+            case 'view all employees': 
+              console.log('I made the connection!!!employees');
+              break;
+            case 'add a department': 
+              console.log('I made the connection!!!add a department');              
+              break;
+            case 'add a role': 
+              console.log('I made the connection!!!add a role');
+              break;
+            case 'add an employee': 
+              console.log('I made the connection!!!add an employee');
+              break;
+            case 'update an employee role': 
+              console.log('I made the connection!!!update an employee role');
+              break;
+            default:
+              console.log('Almost there');
+          };
+          //testInq();
   });
+};
+
+function timer(){
+    return new Promise(resolve => {
+        setTimeout(() => {
+          inqStart();
+        }, 0); //0 sec delay
+    });
+}
+
+
+inqStart();  // <-- Everything starts here
+
 ////////////////////////
 
-output = () => {
+dbAllDept = () => {
     console.log('Select all the department...\n');
     connection.query('SELECT * FROM department', function(err, res) {
         if(err) throw err;
