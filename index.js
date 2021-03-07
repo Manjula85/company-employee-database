6e04ba66581f4cef6135d3e61c3de836cff1899f
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2021-03-06 19:11:10 -0500

    submitting

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql2/promise");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
-//const dbAllDept = require('./dbQueries/queries');
 
 let connection = null;
 //Create the connection to database
@@ -35,8 +34,10 @@ const inqStart = function () {
           "add a role",
           "add an employee",
           "update an employee role",
+          "view employee by manager",
+          "view employee by department",
           "delete a department",
-          "exit" /*EXTRA*/,
+          "exit",
         ],
       },
     ])
@@ -70,39 +71,44 @@ const inqStart = function () {
           console.log("I made the connection!!!update an employee role");
           dbUpdateEmployeeRole();
           break;
+        case "view employee by manager":
+          console.log("I made the connection!!!view employee by manager");
+          dbViewEmployeeByManager();
+          break;
+        case "view employee by department":
+          console.log("I made the connection!!!view employee by department");
+          dbViewEmployeeByDepartment();
+          break;
         case "delete a department":
-          console.log("I made the connection!!!update an employee role");
+          console.log("I made the connection!!!remove a department");
           dbDeleteDept();
           break;
-        default:
-          console.log("Almost there");
+        case "delete a role":
+          console.log("I made the connection!!!remove a role");
+          dbDeleteRole();
+          break;
+        case "delete an employee":
+          console.log("I made the connection!!!remove an employee");
+          dbDeleteEmployee();
+          break;
+        case "exit":
+          console.log("Good bye");
           dbConnectionEnd();
+          break;
       }
     });
 };
-
+//starts the connection
 main();
+
 inqStart(); // <-- Everything starts here
 
-////////////////////////
-
-const dbAllRoles = async () => {
-  console.log("Select all the roles...");
-
-  try {
-    const [rows, fields] = await connection.execute("SELECT * FROM roles");
-    console.log("\n");
-    //Show all the results
-    console.table(rows);
-  } catch (error) {
-    console.log("Error: " + error);
-  }
-
-  inqStart();
+//END connection
+const dbConnectionEnd = () => {
+  connection.close();
 };
 
-const dbConnectionEnd = () => {};
-
+//View all Departments
 const dbAllDept = async () => {
   console.log("Select all the departments...");
 
@@ -117,11 +123,28 @@ const dbAllDept = async () => {
   inqStart();
 };
 
+//View all Roles
+dbAllRoles = async () => {
+  console.log("Select all the roles...");
+
+  try {
+    const [rows] = await connection.execute("SELECT * FROM roles");
+    console.log("\n");
+    //Show all the results
+    console.table(rows);
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+
+  inqStart();
+};
+
+//View all Employees
 const dbAllEmployees = async () => {
   console.log("Select all the employees...");
 
   try {
-    const [rows, fields] = await connection.execute("SELECT * FROM employee");
+    const [rows] = await connection.execute("SELECT * FROM employee");
     console.log("\n");
     console.table(rows);
   } catch (error) {
@@ -135,7 +158,6 @@ const dbAllEmployees = async () => {
 const dbAddDept = async () => {
   console.log("Add a new dept...");
 
-  /////
   await inquirer
     .prompt([
       {
@@ -159,19 +181,203 @@ const dbAddDept = async () => {
         console.log("Error: " + error);
       }
     });
-  /////
 
   inqStart();
 };
 
-const dbDeleteDept = async ()=> {
+//Add a new Role
+const dbAddRole = async () => {
+  console.log("Add a new role...");
 
   await inquirer
     .prompt([
       {
         type: "input",
+        name: "title",
+        message: "Enter title here: ",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "Enter salary here: ",
+      },
+      {
+        type: "input",
+        name: "dept_id",
+        message: "Enter department id here: ",
+      },
+    ])
+    .then(async ({ title, salary, dept_id }) => {
+      try {
+        const [
+          rows,
+        ] = await connection.execute(
+          "INSERT INTO roles (title, salary, department_id) VALUES (?,?,?);",
+          [title, salary, dept_id]
+        );
+        console.log("\n");
+        console.table(rows);
+      } catch (error) {
+        console.log("Error: " + error);
+      }
+    });
+
+  inqStart();
+};
+
+//Add a new Employee
+const dbAddEmployee = async () => {
+  console.log("Add a new role...");
+
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "id",
+        message: "Enter employee id here: ",
+      },
+      {
+        type: "input",
+        name: "first_name",
+        message: "Enter first name here: ",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "Enter last name id here: ",
+      },
+      {
+        type: "input",
+        name: "roles_id",
+        message: "Enter role id here: ",
+      },
+      {
+        type: "input",
+        name: "manager_id",
+        message: "Enter manager's id here: ",
+      },
+    ])
+    .then(async ({ id, first_name, last_name, roles_id, manager_id }) => {
+      try {
+        const [
+          rows,
+        ] = await connection.execute(
+          "INSERT INTO employee (id,first_name, last_name, roles_id, manager_id) VALUES (?,?,?,?,?);",
+          [id, first_name, last_name, roles_id, manager_id]
+        );
+        console.log("\n");
+        console.table(rows);
+      } catch (error) {
+        console.log("Error: " + error);
+      }
+    });
+
+  inqStart();
+};
+
+//Update an Employee role
+const dbUpdateEmployeeRole = async () => {
+  console.log("Update an employee role...");
+
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "Enter employee's first here: ",
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "Enter New title/role here: ",
+      },
+    ])
+    .then(async ({ first_name, title }) => {
+      try {
+        const [rows] = await connection.execute(
+          `UPDATE roles 
+           LEFT JOIN employee ON roles.id=employee.id
+           SET title = '${title}'
+           WHERE first_name = '${first_name}';`
+        );
+        console.log("\n");
+        console.table(rows);
+      } catch (error) {
+        console.log("Error: " + error);
+      }
+    });
+
+  inqStart();
+};
+
+//View employees by Manager
+const dbViewEmployeeByManager = async () => {
+  console.log("View employee by manager...");
+
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "manager_id",
+        message: "Enter manager id here: ",
+      },
+    ])
+    .then(async ({ manager_id }) => {
+      try {
+        const [rows] = await connection.execute(
+          `SELECT * FROM employee
+           WHERE manager_id = '${manager_id}';`
+        );
+        console.log("\n");
+        console.table(rows);
+      } catch (error) {
+        console.log("Error: " + error);
+      }
+    });
+
+  inqStart();
+};
+
+//View employees by Department
+const dbViewEmployeeByDepartment = async () => {
+  console.log("View employee by department...");
+
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "dep_name",
+        message: "Enter department name here: ",
+      },
+    ])
+    .then(async ({ dep_name }) => {
+      try {
+        const [rows] = await connection.execute(
+          `SELECT 
+          r.*
+          FROM roles r
+          RIGHT JOIN employee e ON e.roles_id = r.id
+          LEFT JOIN department d ON d.id = r.department_id
+          WHERE manager_id = '${dep_name}';`
+        );
+        console.log("\n");
+        console.table(rows);
+      } catch (error) {
+        console.log("Error: " + error);
+      }
+    });
+
+  inqStart();
+};
+
+//Delete a Department
+const dbDeleteDept = async () => {
+  await inquirer
+    .prompt([
+      {
+        type: "input",
         name: "departmentDel",
-        message: "Which department are you deleting? ",
+        message: "Which department are you removing? ",
       },
     ])
     .then(async ({ departmentDel }) => {
@@ -179,10 +385,64 @@ const dbDeleteDept = async ()=> {
         const [
           rows,
           fields,
-        ] = await connection.execute(
-          `DELETE FROM department WHERE names=?`,
-          [departmentDel]
-        );
+        ] = await connection.execute(`DELETE FROM department WHERE names=?`, [
+          departmentDel,
+        ]);
+        console.log("\n");
+        console.table(rows);
+      } catch (error) {
+        console.log("Error: " + error);
+      }
+    });
+
+  inqStart();
+};
+
+//Delete a Role
+const dbDeleteRole = async () => {
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "roleDel",
+        message: "Which role are you removing? ",
+      },
+    ])
+    .then(async ({ roleDel }) => {
+      try {
+        const [
+          rows,
+          fields,
+        ] = await connection.execute(`DELETE FROM roles WHERE names=?`, [
+          roleDel,
+        ]);
+        console.log("\n");
+        console.table(rows);
+      } catch (error) {
+        console.log("Error: " + error);
+      }
+    });
+
+  inqStart();
+};
+
+//Remove an Employee
+const dbDeleteEmployee = async () => {
+  await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "employeeDel",
+        message: "Which employee are you removing? ",
+      },
+    ])
+    .then(async ({ employeeDel }) => {
+      try {
+        const [
+          rows,
+        ] = await connection.execute(`DELETE FROM employee WHERE names=?`, [
+          employeeDel,
+        ]);
         console.log("\n");
         console.table(rows);
       } catch (error) {
